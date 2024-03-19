f3c3a1cf69e7e623970d50aa222282f33c609965
Martinekk <martinmatostano@gmail.com>
2024-03-19 21:43:46 +0100

    Initial commit

---
+++ src/main/java/sk/metinas/jobarea/JobareaApplication.java
@@ -0,0 +1,14 @@
+package sk.metinas.jobarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobareaApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
